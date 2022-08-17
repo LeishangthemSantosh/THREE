f96b8a080c45f95dab5cb156d3b69130706df93e
Leishangthem Santosh <santoshleishangthem1997@gmail.com>
2022-08-18 01:55:17 +0530

    rotation

--- main.js
+++ main.js
@@ -3,12 +3,50 @@ import * as THREE from 'three'
 
 const scene = new THREE.Scene()
 
-const geometry = new THREE.BoxGeometry(1,1,1)
-const material = new THREE.MeshBasicMaterial({color:0x00ffff})
+// const geometry = new THREE.BoxGeometry(1,1,1)
+// const material = new THREE.MeshBasicMaterial({color:0x00ffff})
 
-const mesh = new THREE.Mesh(geometry, material)
+// const mesh = new THREE.Mesh(geometry, material)
+// mesh.position.x = 0.7
+// mesh.position.y = -0.6
+// mesh.position.z = 1
+// mesh.position.set(0.7, -0.6, 1)
 
-scene.add(mesh)
+// mesh.scale.x = 1
+// mesh.scale.x = 1
+// mesh.scale.x = 1
+// mesh.scale.set(2, 0.5, 0.5)
+// mesh.rotation.reorder('YXZ')
+// mesh.rotation.x = Math.PI * 0.25
+// mesh.rotation.y = Math.PI * 0.25
+
+// scene.add(mesh)
+
+const group = new THREE.Group()
+scene.add(group)
+
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({color:0x00ffff})
+)
+cube1.position.x = -1.5
+group.add(cube1)
+
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({color:0x00ffff})
+)
+cube2.position.x = 0
+group.add(cube2)
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({color:0x00ffff})
+)
+cube3.position.x = 1.5
+group.add(cube3)
+group.rotation.y = 1
+
+// console.log(mesh.position.length())
 
 const sizes = {
   width: window.innerWidth,
@@ -17,9 +55,15 @@ const sizes = {
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height)
 
+// camera.position.x = 1
+// camera.position.y = 1
 camera.position.z = 3
 
 scene.add(camera)
+
+// camera.lookAt(mesh.position)
+
+// console.log(mesh.position.distanceTo(camera.position))
 
 const canvas = document.querySelector('.webgl')
 const renderer = new THREE.WebGLRenderer({
